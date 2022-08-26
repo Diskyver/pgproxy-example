55e2abd436eea7a1ffd6a3f67a145357d59a93f1
Maximilien DI DIO <74690920+prx0@users.noreply.github.com>
2022-08-26 16:51:04 +0200

    Add an example of proxified postgresql server program

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/diskyver/pgproxy"
+	"github.com/jackc/pgproto3/v2"
+	"github.com/jackc/pgx/v4"
+)
+
+var options struct {
+	proxyAddr string
+	pgAddr    string
+}
+
+func pgAddr() (string, error) {
+	if addr := os.Getenv("PGPROXY_DB_URL"); addr != "" {
+		return addr, nil
+	}
+
+	if addr := options.pgAddr; addr != "" {
+		return addr, nil
+	}
+
+	return "", errors.New("postgresql url not provided")
+}
+
+type Session struct{}
+
+func (s *Session) OnConnect(_ *pgx.Conn) {
+	fmt.Println("I'm in!")
+}
+
+func (s *Session) OnQuery(query *pgproto3.Query) (*pgproto3.Query, error) {
+	fmt.Println("query:", query.String)
+	return query, nil
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage:  %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&options.proxyAddr, "proxy-addr", "127.0.0.1:15432", "Proxy address")
+	flag.StringVar(&options.pgAddr, "pg-addr", "127.0.0.1:5432", "Postgresql url. Also the PGPROXY_DB_URL environment variable exists")
+	flag.Parse()
+
+	dbAddr, err := pgAddr()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := pgproxy.CreatePgProxy(dbAddr, &Session{})
+	proxy.Listen(options.proxyAddr)
+}
